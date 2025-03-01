55ce3a87fe4e072c2166bfe47edab7bd5ef656d9
awesomebrick <baberd@slu.edu>
2025-02-28 20:24:11 -0600

    fishing rod barely works now

--- fishingcast.gd
+++ fishingcast.gd
@@ -1,11 +1,20 @@
 extends Node
 
+var isCast
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	isCast = false
 	pass # Replace with function body.
 
+func _input(event):
+	if event.is_action_pressed("spaceBar"):
+		castLine(isCast) # run the code for casting a line.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func castLine(isCast) -> void:
+	if isCast == false:
+		$player/fishingRod.show()
+		pass
+	if isCast == true:
+		$player/fishingRod.hide()
+		pass
