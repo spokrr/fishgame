ec2eca8a45ddc6c79d7235bcb0db67922c0e2ec2
awesomebrick <baberd@slu.edu>
2025-02-28 23:25:44 -0600

    level changer works now

---
+++ visibilitykillcollision.gd
@@ -0,0 +1,21 @@
+extends StaticBody2D
+
+func _ready() -> void:
+	#_on_visible_changed(visible)
+	pass
+
+func cycle_visible(visibility):
+	print("visibility changed")
+	self.visible = visibility
+	if not visible:
+		for shape in get_children():
+			if shape is CollisionPolygon2D:
+				shape.set_deferred("disabled",true)
+				print("visibility changed")
+				print(shape)
+	else:
+		for shape in get_children():
+			if shape is CollisionPolygon2D:
+				shape.set_deferred("disabled",false)
+				print("visibility changed")
+				print(shape)
